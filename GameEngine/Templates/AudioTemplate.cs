e5fa267d4520e7c694da6b668695927352c0dae1
Miles Izzo <Miles Izzo>
2017-03-29 01:49:28 +1100

    Moved game engine into its own directory

---
+++ GameEngine/Templates/AudioTemplate.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework.Audio;
+
+namespace GameEngine.Templates
+{
+    public class AudioTemplate : ITemplate
+    {
+        private readonly string name;
+        public readonly SoundEffect Audio;
+
+        public AudioTemplate(string name, SoundEffect audio)
+        {
+            this.name = name;
+            this.Audio = audio;
+        }
+
+        public string Name { get { return this.name; } }
+    }
+}

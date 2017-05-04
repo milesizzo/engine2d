db30c4bc00b0fea2ce51956d33c394aa5ae5e3d4
Miles Izzo <mgi@mgi.net.au>
2017-05-05 01:16:53 +1000

    Added Mouse Helper

---
+++ GameEngine/Helpers/MouseHelper.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngine.Helpers
+{
+    public enum MouseButton
+    {
+        Left,
+        Right,
+        Middle,
+    }
+
+    public static class MouseHelper
+    {
+        private static Dictionary<MouseButton, bool> buttonHeld = new Dictionary<MouseButton, bool>();
+        private static int? scrollValue = null;
+
+        public static bool ButtonPressed(MouseButton button)
+        {
+            var mouse = Mouse.GetState();
+            var state = ButtonState.Released;
+            switch (button)
+            {
+                case MouseButton.Left:
+                    state = mouse.LeftButton;
+                    break;
+                case MouseButton.Middle:
+                    state = mouse.MiddleButton;
+                    break;
+                case MouseButton.Right:
+                    state = mouse.RightButton;
+                    break;
+            }
+            if (state == ButtonState.Pressed)
+            {
+                if (!buttonHeld.ContainsKey(button))
+                {
+                    buttonHeld[button] = true;
+                    return true;
+                }
+            }
+            else
+            {
+                buttonHeld.Remove(button);
+            }
+            return false;
+        }
+
+        public static int ScrollDirection
+        {
+            get
+            {
+                var value = Mouse.GetState().ScrollWheelValue;
+                if (scrollValue == null)
+                {
+                    scrollValue = value;
+                }
+                else if (value > scrollValue)
+                {
+                    scrollValue = value;
+                    return 1;
+                }
+                else if (value < scrollValue)
+                {
+                    scrollValue = value;
+                    return -1;
+                }
+                return 0;
+            }
+        }
+    }
+}
